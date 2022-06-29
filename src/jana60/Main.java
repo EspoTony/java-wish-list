df00d9e9d663b34b5beb02e055d04d855592360e
EspoTony <espotony98@gmail.com>
2022-06-29 12:42:17 +0200

    Aggiunta limite lista

--- src/jana60/Main.java
+++ src/jana60/Main.java
@@ -1,6 +1,7 @@
 package jana60;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ public class Main {
 		boolean flag = false;
 		String conferma;
 		
-		List<String> listaDiDesideri = new ArrayList<String>();
+		List<String> listaDiDesideri = new ArrayList<String>(3);
 		
 		do {
 			
@@ -22,27 +23,33 @@ public class Main {
 			listaDiDesideri.add(scan.nextLine());
 			System.out.println("La lista ora contiene " + listaDiDesideri.size() + " desideri.");
 			
+			if(listaDiDesideri.size() == 3)
+				flag =true;
+			else {
 			
-			do {
+				do {
+					
+					System.out.print("Vuoi inserire un nuovo desiderio? ");
+					conferma = scan.nextLine();
 				
-				System.out.print("Vuoi inserire un nuovo desiderio? ");
-				conferma = scan.nextLine();
+					if(!conferma.equalsIgnoreCase("si") && !conferma.equalsIgnoreCase("no"))
+					System.out.println("Rispondi solo Si o No.");
+				
+				}while(!conferma.equalsIgnoreCase("no") && !conferma.equalsIgnoreCase("si"));
+				
+				if(conferma.equalsIgnoreCase("no"))
+					flag = true;
+				
+			}
 			
-				if(!conferma.equalsIgnoreCase("si") && !conferma.equalsIgnoreCase("no"))
-				System.out.println("Rispondi solo Si o No.");
-			
-			}while(!conferma.equalsIgnoreCase("no") && !conferma.equalsIgnoreCase("si"));
-			
-			if(conferma.equalsIgnoreCase("no"))
-				flag = true;
 			
 		}while(!flag);
 
-		for(int i = 0; i < listaDiDesideri.size(); i++) {
-			
-			System.out.println("Il desiderio numero " + (i + 1) + " è " + listaDiDesideri.get(i));
-			
-		}
+		Collections.sort(listaDiDesideri);
+		
+		System.out.println("La tua lista di desideri ordinata è: ");
+		System.out.println(listaDiDesideri);
+		
 		
 	}
 
